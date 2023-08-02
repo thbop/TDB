a3a6ec459cb9946a5a87163d4cd85d7fd597bea0
Thbop <75861321+thbop@users.noreply.github.com>
2023-08-02 12:29:50 -0400

    Now my brain hurts - too many vulnerabilities

--- tdb/handlers/base.py
+++ tdb/handlers/base.py
@@ -1,5 +1,21 @@
 
 class Base:
-    def __init__(self, db):
+    def __init__(self, db, key, base_obj):
         self.db = db
-        
+        self.key = key
+        self.base_obj = base_obj
+    
+    def load(self):
+        try:
+            if isinstance(self.db[self.key], dict):
+                self.objects = {}
+                for k, i in self.db[self.key].items():
+                    obj = self.base_obj.copy()
+                    obj.load(i)
+                    self.objects[k] = obj
+            elif isinstance(self.db[self.key], list):
+                pass
+            else:
+                raise ValueError('Item must be either a list or a dict')
+        except KeyError:
+            raise KeyError(f'The key "{self.key}" does not exist in the database, either remove this handler or create the key')
--- tdb/obj/base.py
+++ tdb/obj/base.py
@@ -1,13 +1,29 @@
 from ..utils.encryption import encrypt, decrypt
 import json
+from copy import copy
 
 class Base:
     def __init__(self):
         self.private = {}
+        self._encrypted = True
+
         self.public = {}
 
+    def load(self, i):
+        self.private = i['private']
+        self.public = i['public']
+
     def get_private(self, key, data):
-        self.private = json.loads(decrypt(key, data))
+        if not self._encrypted:
+            self.private = json.loads(decrypt(key, data))
+            self._encrypted = False
 
     def set_private(self, key):
-        return encrypt(key, json.dumps(self.private))
+        if self._encrypted:
+            self.private = encrypt(key, json.dumps(self.private))
+            self._encrypted = True
+    
+    
+
+    def copy(self):
+        return copy(self)
